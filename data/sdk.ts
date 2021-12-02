04e1784ef517f61017bb4643c605f9a97a341a13
David Mihal <david@dmihal.com>
2021-12-02 17:09:04 +0000

    Switch subgraph

--- data/sdk.ts
+++ data/sdk.ts
@@ -6,7 +6,7 @@ if (!process.env.MORALIS_KEY) {
 
 const sdk = new CryptoStatsSDK({
   moralisKey: process.env.MORALIS_KEY,
-  adapterListSubgraph: 'dmihal/stateless-list-registry-kovan-staging',
+  adapterListSubgraph: 'dmihal/cryptostats-adapter-registry-test',
 })
 
 export default sdk
