b7d19d30b088dad75b634566d9caa33c5e39ec9c
David Mihal <david@dmihal.com>
2022-04-01 15:38:58 +0300

    Switch to Alchemy

--- data/sdk.ts
+++ data/sdk.ts
@@ -1,12 +1,18 @@
 import { CryptoStatsSDK } from '@cryptostats/sdk'
 
-if (!process.env.MORALIS_KEY) {
-  console.error('Moralis key missing')
-}
+// if (!process.env.MORALIS_KEY) {
+//   console.error('Moralis key missing')
+// }
 
 const sdk = new CryptoStatsSDK({
-  moralisKey: process.env.MORALIS_KEY,
-  adapterListSubgraph: 'dmihal/cryptostats-adapter-registry-test',
+  // moralisKey: process.env.MORALIS_KEY,
 })
+
+if (process.env.ALCHEMY_ETH_KEY) {
+  const rpc = `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_ETH_KEY}`
+  sdk.ethers.addProvider('ethereum', rpc, { archive: true })
+} else {
+  console.error('Alchemy key not set')
+}
 
 export default sdk
